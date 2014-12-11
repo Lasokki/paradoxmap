5ab6f051f3e59656df98c7f04cdcc0d72a55936b
Erkki Mattila <lasokki@gmail.com>
2014-12-11 16:16:26 +0200

    Came up with a draft of the algorithm

--- marcher/marcher.py
+++ marcher/marcher.py
@@ -1,6 +1,17 @@
+"""
+A program for generating a GeoJSON file from RGB-bitmap.
+Author: Erkki Mattila
+"""
+
 import Image
+
+# Draft for algorithm:
+# Have all possible province colours in a stack
+# Iterate through the pixels. If pixel is non-white and in the stack, start marching.
+# After the province is traced, add the resulting path to output. Remove the colour from stack.
+# Continue iteration until the end of bitmap or when stack is empty.
  
-#img = Image.open("/home/lasokki/projects/paradoxmap/imgs/provinces.bmp")
+#img = Image.open("/home/lasokki/projects/map_project/imgs/provinces.bmp")
 img = Image.open("surrounded.bmp")
 
 pixels = img.load() # create the pixel map
