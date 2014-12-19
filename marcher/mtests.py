fb1f4c8e6e3ea08ba99f2c788c6978b077ff36bc
Erkki Mattila <lasokki@gmail.com>
2014-12-19 19:11:02 +0200

    Added tests

--- marcher/mtests.py
+++ marcher/mtests.py
@@ -6,27 +6,47 @@ import Image
 from marcher import Marcher
 
 def test():
-    #msurr = Marcher("surrounded.bmp"
-    #mjoin = Marcher("joined.bmp")
+
+
+    surrounded = Image.open("surrounded.bmp")
+    surr_pix = surrounded.load()
 
     separate = Image.open("separate.bmp")
-    pixels = separate.load()
+    sepa_pix = separate.load()
+
+    joined = Image.open("joined.bmp")
+    join_pix = joined.load()
 
     # blue
-    s_colours = [(0,0,255)]
+    colours = [(0,0,255)]
 
-    msepa = Marcher("separate.bmp", s_colours)
+    msepa = Marcher("separate.bmp", colours)
+    msurr = Marcher("surrounded.bmp", colours)
+    mjoin = Marcher("joined.bmp", colours)
 
-    points = msepa.do_march()
-    
-    for p in points:
-        print p
+    sepa_points = msepa.do_march()
+    surr_points = msurr.do_march()
+    join_points = mjoin.do_march()
+
+    for p in sepa_points:
+         x = p[0]
+         y = p[1]
+         sepa_pix[x,y] = (255,0,0)
+
+    for p in surr_points:
         x = p[0]
         y = p[1]
-        pixels[x,y] = (255,0,0)
+        surr_pix[x,y] = (255,0,0)
+
+    for p in join_points:
+        x = p[0]
+        y = p[1]
+        join_pix[x,y] = (255,0,0)
 
     separate.show()
-    
+    surrounded.show()
+    joined.show()
+
 if __name__ == "__main__":
     print "Start tests"
     test()
