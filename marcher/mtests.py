e866315aae817759698a7b5007837b274133cade
Erkki Mattila <lasokki@gmail.com>
2014-12-20 17:55:26 +0200

    Split tests to run separately

--- marcher/mtests.py
+++ marcher/mtests.py
@@ -18,7 +18,7 @@ def test():
     joined = Image.open("test_input/joined.bmp")
     join_pix = joined.load()
 
-    edges = Image.open("test_input/complex.bmp")
+    edges = Image.open("test_input/edges.bmp")
     edge_pix = edges.load()
 
     blue = (0,0,255)
@@ -29,45 +29,64 @@ def test():
     msurr = Marcher("test_input/surrounded.bmp")
     msepa = Marcher("test_input/separate.bmp")
     mjoin = Marcher("test_input/joined.bmp")
-    medge = Marcher("test_input/complex.bmp")
+    medge = Marcher("test_input/edges.bmp")
 
+    print "Start of surrounded.bmp"
     for colour in colours:
 
         msurr.colour = colour
-        msepa.colour = colour
-        mjoin.colour = colour
-        medge.colour = colour
-
-        sepa_points = msepa.do_march()
         surr_points = msurr.do_march()
-        join_points = mjoin.do_march()
-        edge_points = medge.do_march()
-
-        for p in sepa_points:
-            x = p[0]
-            y = p[1]
-            sepa_pix[x,y] = (255,0,0)
 
         for p in surr_points:
             x = p[0]
             y = p[1]
             surr_pix[x,y] = (255,0,0)
 
+    surrounded.save("test_output/surrtest.bmp")
+    print "surrounded.bmp complete"
+
+    print "Start of separate.bmp"
+    for colour in colours:
+
+        msepa.colour = colour
+        sepa_points = msepa.do_march()
+        
+        for p in sepa_points:
+            x = p[0]
+            y = p[1]
+            sepa_pix[x,y] = (255,0,0)
+
+    separate.save("test_output/sepatest.bmp")
+    print "separate.bmp complete"
+
+    print "Start of joined.bmp"
+    for colour in colours:
+
+        mjoin.colour = colour
+        join_points = mjoin.do_march()
+
         for p in join_points:
             x = p[0]
             y = p[1]
             join_pix[x,y] = (255,0,0)
+    
+    joined.save("test_output/jointest.bmp")
+    print "joined.bmp complete"
+
+    "Start of edges.bmp"
+    for colour in colours:
+        
+        medge.colour = colour
+        edge_points = medge.do_march()
 
         for p in edge_points:
             x = p[0]
             y = p[1]
             edge_pix[x,y] = (175,175,175)
 
-    separate.save("test_output/sepatest.bmp")
-    surrounded.save("test_output/surrtest.bmp")
-    joined.save("test_output/jointest.bmp")
     edges.save("test_output/edgetest.bmp")
-    
+    print "edges.bmp complete"
+
 if __name__ == "__main__":
     print "Start tests"
     test()
