40f80827bb2755c89de496bfeb141b45ddcfaf81
Erkki Mattila <lasokki@gmail.com>
2015-01-04 19:09:50 +0200

    Modified print for missing arguments

--- marcher/mtests.py
+++ marcher/mtests.py
@@ -66,4 +66,4 @@ if __name__ == "__main__":
         test(sys.argv[1:])
         print "Tests have finished"
     else:
-        print "Give some tests"
+        print "Give some tests: sur, sep, joi, edg"
