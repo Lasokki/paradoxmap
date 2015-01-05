01579e9239ad15901fa2ba8c50938255980c2b27
Erkki Mattila <lasokki@gmail.com>
2015-01-05 19:03:16 +0200

    Refactored tests to be more concise

--- marcher/mtests.py
+++ marcher/mtests.py
@@ -25,32 +25,29 @@ def test(tests):
     for test in tests:
         colours = simple_test_colours
         if test == "sur":
-            img = Image.open("test_input/surrounded.bmp")
-            pix = img.load()
-            marcher = Marcher("test_input/surrounded.bmp")
+            infile = "test_input/surrounded.bmp"
             out = "test_output/surrtest.bmp"
         elif test == "sep":
-            img = Image.open("test_input/separate.bmp")
-            pix = img.load()
-            marcher = Marcher("test_input/separate.bmp")
+            infile = "test_input/separate.bmp"
             out = "test_output/sepatest.bmp"
         elif test == "joi":
-            img = Image.open("test_input/joined.bmp")
-            pix = img.load()
-            marcher = Marcher("test_input/joined.bmp")
+            infile = "test_input/joined.bmp"
             out = "test_output/jointest.bmp"
         elif test == "edg":
-            img = Image.open("test_input/edges.bmp")
-            pix = img.load()
-            marcher = Marcher("test_input/edges.bmp")
+            infile = "test_input/edges.bmp"
             out = "test_output/edgetest.bmp"
             colours = edge_test_colours
         elif test == "squ":
-            img = Image.open("test_input/squiggles.bmp")
-            pix = img.load()
-            marcher = Marcher("test_input/squiggles.bmp")
+            infile = "test_input/squiggles.bmp"
             out = "test_output/squitest.bmp"
             colours = squi_test_colours
+        else:
+            print "no test given"
+            break
+
+        img = Image.open(infile)
+        pix = img.load()
+        marcher = Marcher(infile)
 
         for colour in colours:
             marcher.colour = colour
