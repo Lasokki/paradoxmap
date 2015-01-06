20ee96089f3c805d5734cb783aab0a006a651f1b
Erkki Mattila <lasokki@gmail.com>
2015-01-06 20:49:57 +0200

    Added read_definition(), prints and time keeping

--- marcher/generate.py
+++ marcher/generate.py
@@ -6,18 +6,48 @@ This file runs Marchers.
 Erkki Mattila, 2014
 """
 
-import Image, time
+import Image, time, csv
 from marcher import Marcher
 
+def read_definition(definition):
+
+    start = time.time()
+    print "Reading definition.csv"
+
+    provs = []
+
+    csv.register_dialect('ckii', delimiter=';', quoting=csv.QUOTE_NONE)
+
+    with open(definition, 'rb') as f:
+        reader = csv.reader(f, 'ckii')
+
+        for row in reader:
+            if len(row) > 3 and row[0] != '':
+                try:
+                    r = int(row[1])
+                    g = int(row[2])
+                    b = int(row[3])
+                    provs.append((r,g,b))
+                except ValueError:
+                    pass
+
+    delta = time.time() - start
+    print ("Reading definitions took %.3f seconds" %delta)
+    return provs
+
 def generate():
+
+    print ("Begun generating image")
+    start = time.time()
 
     img = Image.open("provinces.bmp")
     pix = img.load()
     marcher = Marcher("provinces.bmp")
     out = "test_output/prov_out.bmp"
-    colours = [(42,3,128)]
+    provs = read_definition("definition.csv")
 
-    for colour in colours:
+    for colour in provs:
+        print colour
         marcher.colour = colour
         points = marcher.do_march()
         for p in points:
@@ -25,8 +55,8 @@ def generate():
             y = p[1]
             pix[x,y] = (255,0,0)
 
-        img.save(out)
-        
+    img.save(out)
+
 if __name__ == "__main__":
     start = time.clock()
     generate()
