42401243f1e80d4c2f697f5e2a6555a2f67464b1
Lasokki <lasokki@gmail.com>
2015-02-14 02:57:04 +0200

    Outputs now cultures.js, containing provid:culture -pairs

--- marcher/history_generator.py
+++ marcher/history_generator.py
@@ -1,24 +1,70 @@
+# -*- coding: utf-8 -*-
+
 """This script will output a JSON which contains information about holders of provinces.
 Folders to check:
 /history/provinces/
 /history/characters/
 /history/titles
 """
+from os import listdir, path
+import time, re, json
 
 def read_provinces():
-    pass
+    date_re = re.compile('\Aculture')
+
+    cultures = {}
+
+    for prov in listdir("history/provinces"):
+        prov_id = (prov.split(' '))[0]
+        provp = path.join("history/provinces", prov)
+
+        f = open(provp)
+
+        culture = None
+        for line in f:
+            if re.search(date_re, line):
+                tmp = line.split(' = ')
+                tmp = tmp[1].split('#')
+                culture = tmp[0].strip()
+                break
+
+        if culture is not None:
+            cultures[prov_id] = culture
+
+    for key in cultures:
+        print (key, cultures[key])
+
+    with open('cultures.js', 'w') as f:
+        json.dump(cultures, f)
 
 def read_characters():
     pass
 
 def read_titles():
-    pass
+    
+    date_re = re.compile('\d{3}.\d{2}.\d{2}')
+
+    for title in listdir("history/titles"):
+        derp = path.join("history/titles", title)
+
+        try:
+            f = open(derp)
+            for line in f:
+                if re.search(date_re, line):
+                    print(line)
+        except UnicodeDecodeError:
+            f = open(derp, encoding="utf-8")
+            for line in f:
+                if re.search(date_re, line):
+                    print(line)
+
+
 
 def generate():
     #Collect data
     read_provinces()
-    read_characters()
-    read_titles()
+    #read_characters()
+    #read_titles()
 
     #Add them all together
     pass
