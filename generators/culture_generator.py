a2bd012c48a29d35f4c513ef36eb1ff698286fb8
Erkki Mattila <lasokki@gmail.com>
2015-02-15 14:24:17 +0200

    Now parses colours for cultures

--- generators/culture_generator.py
+++ generators/culture_generator.py
@@ -12,13 +12,34 @@ def read_culture_colours():
     colours = {}
 
     f = open("00_cultures.txt")
-    skip_re = re.compile('graphical')
+    graph_re = re.compile('graphical')
+    curly_re = re.compile('\s}')
+    color_re = re.compile('color');
 
     for line in f:
         tabs = len(line) - len(line.lstrip('\t'))
-        
-        if tabs == 1 and not re.search(skip_re, line):
-            print line
+        colour = None
+        if tabs == 1 and not re.search(graph_re, line) and not re.match(curly_re, line):
+            tmp = line.split(' = ')
+            culture = tmp[0].strip()
+
+        if tabs == 2 and re.search(color_re, line):
+            tmp = line.split(' = ')
+            s = tmp[1].strip()
+            s = s.strip('{} ')
+            cl = s.split(' ')
+            colour = "rgb("
+            colour = colour + str((int(255 * float(cl[0]))))
+            colour = colour + "," + str((int(255 * float(cl[1]))))
+            colour = colour + "," + str((int(255 * float(cl[2])))) + ")"
+            print colour
+
+        if colour is not None:
+            colours[culture] = colour
+
+    with open('cultures.js', 'w') as f:
+        f.write("var culture_colours = ") 
+        json.dump(colours, f, separators=(',',':'))
 
 def read_cultures_from_provinces():
     date_re = re.compile('\Aculture')
@@ -42,16 +63,13 @@ def read_cultures_from_provinces():
         if culture is not None:
             cultures[prov_id] = culture
 
-    for key in cultures:
-        print (key, cultures[key])
-
-    with open('cultures.js', 'w') as f:
-        f.write("var cultures = ") 
+    with open('cultures.js', 'a') as f:
+        f.write(";var cultures = ") 
         json.dump(cultures, f, separators=(',',':'))
 
 def generate():
-    read_cultures_from_provinces()
     read_culture_colours()
+    read_cultures_from_provinces()
 
 if __name__ == "__main__":
     start = time.clock()
